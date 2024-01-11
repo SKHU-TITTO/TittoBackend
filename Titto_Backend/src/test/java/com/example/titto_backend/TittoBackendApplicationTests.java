e70dd9826c59a0dcb0326e608297b43e45ab5753
김동균 <dongkyun@gimdong-gyun-ui-MacBookAir.local>
2024-01-11 19:31:12 +0900

    스프링 커밋

---
+++ Titto_Backend/src/test/java/com/example/titto_backend/TittoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.titto_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TittoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
