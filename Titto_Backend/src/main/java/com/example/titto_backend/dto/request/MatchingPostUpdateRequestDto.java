d573f5732798ba7683d1fddf8e611be0c95c518b
dongkyun0713 <kyun010713@gmail.com>
2024-01-15 18:38:09 +0900

    feat(domain) : 카테고리 변수 추가

--- Titto_Backend/src/main/java/com/example/titto_backend/dto/request/MatchingPostUpdateRequestDto.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/dto/request/MatchingPostUpdateRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.titto_backend.dto.request;
 
+import com.example.titto_backend.domain.MatchingBoard.Category;
 import com.example.titto_backend.domain.MatchingBoard.MatchingPost;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -13,7 +14,11 @@ public class MatchingPostUpdateRequestDto {
     @NotNull
     private Long userId;
     @NotNull
+    private Category category;
+
+    @NotNull
     private String title;
+
     @NotNull
     private String content;
 
