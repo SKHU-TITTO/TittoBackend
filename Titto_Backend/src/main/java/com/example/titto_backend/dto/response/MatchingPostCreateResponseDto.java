5c5dd3f7aae56ea89be03d95192da31a013bc968
dongkyun0713 <kyun010713@gmail.com>
2024-01-15 20:18:00 +0900

    feat(MatchingPostCreateResponseDto) : 생성날짜 추가

--- Titto_Backend/src/main/java/com/example/titto_backend/dto/response/MatchingPostCreateResponseDto.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/dto/response/MatchingPostCreateResponseDto.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -18,8 +20,17 @@ public class MatchingPostCreateResponseDto {
     private String title;
     private String content;
     private Integer viewCount;
+    private LocalDateTime createDate;
 
-    public static MatchingPostCreateResponseDto of(Long matchingPostId, UserResponseDto user, Category category, Status status, String title, String content, Integer viewCount) {
-        return new MatchingPostCreateResponseDto(matchingPostId, user, category, status, title, content, viewCount);
+    public static MatchingPostCreateResponseDto of(
+            Long matchingPostId,
+            UserResponseDto user,
+            Category category,
+            Status status,
+            String title,
+            String content,
+            Integer viewCount,
+            LocalDateTime createDate) {
+        return new MatchingPostCreateResponseDto(matchingPostId, user, category, status, title, content, viewCount, createDate);
     }
 }
