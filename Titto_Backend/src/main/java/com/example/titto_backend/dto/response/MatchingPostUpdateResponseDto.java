1b1d43bb8eed4035255c49c651d7660a9c4d29c0
dongkyun0713 <kyun010713@gmail.com>
2024-01-16 19:22:21 +0900

    feat(dto) : 메소드 수정

--- Titto_Backend/src/main/java/com/example/titto_backend/dto/response/MatchingPostUpdateResponseDto.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/dto/response/MatchingPostUpdateResponseDto.java
@@ -1,10 +1,10 @@
 package com.example.titto_backend.dto.response;
 
-import com.example.titto_backend.domain.MatchingBoard.Category;
-import com.example.titto_backend.domain.MatchingBoard.Status;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
@@ -12,21 +12,23 @@ import lombok.NoArgsConstructor;
 public class MatchingPostUpdateResponseDto {
 
     private Long matchingPostId;
-    private UserResponseDto user;
-    private Category category;
-    private Status status;
+    private String author;
+    private String category;
+    private String status;
     private String title;
     private String content;
     private Integer viewCount;
+    private LocalDateTime updateDate;
 
     public static MatchingPostUpdateResponseDto of(
             Long matchingPostId,
-            UserResponseDto user,
-            Category category,
-            Status status,
+            String author,
+            String category,
+            String status,
             String title,
             String content,
-            Integer viewCount) {
-        return new MatchingPostUpdateResponseDto(matchingPostId, user, category, status, title, content, viewCount);
+            Integer viewCount,
+            LocalDateTime updateDate) {
+        return new MatchingPostUpdateResponseDto(matchingPostId, author, category, status, title, content, viewCount, updateDate);
     }
 }
