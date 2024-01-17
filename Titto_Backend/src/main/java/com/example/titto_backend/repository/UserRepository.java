2096162b3db44cc078482749309a8cf87fa23e03
dd-jiyun <dlwldbs8015@gmail.com>
2024-01-18 02:05:36 +0900

    feat: UserRepository(Email)

---
+++ Titto_Backend/src/main/java/com/example/titto_backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.titto_backend.repository;
+
+import com.example.titto_backend.domain.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByEmail(String email);
+}
