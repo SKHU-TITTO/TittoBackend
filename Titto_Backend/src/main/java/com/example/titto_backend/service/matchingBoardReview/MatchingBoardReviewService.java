29a45f72a4cacea0e0c8be29ba8a8d2d470166fb
dongkyunKim <kyun010713@gmail.com>
2024-01-19 22:43:40 +0900

    fix(service) : 패키지 수정

--- Titto_Backend/src/main/java/com/example/titto_backend/service/matchingBoardReview/MatchingBoardReviewService.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/service/matchingBoardReview/MatchingBoardReviewService.java
@@ -1,17 +1,10 @@
-package com.example.titto_backend.sercive.matchingBoardReview;
+package com.example.titto_backend.service.matchingBoardReview;
 
-import com.example.titto_backend.domain.User;
-import com.example.titto_backend.domain.review.MatchingPostReview;
-import com.example.titto_backend.dto.request.matchingPostReviewRequest.MatchingPostReviewCreateRequestDto;
-import com.example.titto_backend.dto.response.matchingPostReviewResponse.MatchingPostReviewCreateResponseDto;
 import com.example.titto_backend.repository.MatchingBoard.MatchingPostRepository;
 import com.example.titto_backend.repository.MatchingPostReviewRepository;
 import com.example.titto_backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.security.Principal;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +14,7 @@ public class MatchingBoardReviewService {
     private final MatchingPostRepository matchingPostRepository;
 
     // 생성
-    @Transactional
+/*    @Transactional
     public MatchingPostReviewCreateResponseDto createReview(Principal principal,MatchingPostReviewCreateRequestDto matchingPostReviewCreateRequestDto) {
         Long userId = Long.valueOf(principal.getName());
         User user = userRepository.findById(userId).orElseThrow(
@@ -37,7 +30,7 @@ public class MatchingBoardReviewService {
                 matchingPostReview.getContent(),
                 matchingPostReview.getCreateDate()
         );
-    }
+    }*/
 
 /*   public List<MatchingPostReviewResponseDto> getAllMatchingBoardReviewsByPostId(Long postId) {
        MatchingPost matchingPost = matchingPostRepository.findById(postId)
