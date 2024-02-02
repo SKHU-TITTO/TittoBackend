8dc15223480604f35d1a524f99198f3f7a728ab2
dongkyunKim <kyun010713@gmail.com>
2024-02-02 16:11:18 +0900

    feat(controller) : MatchingBoardController 추가

---
+++ Titto_Backend/src/main/java/com/example/titto_backend/matchingBoard/controller/MatchingBoardController.java
@@ -0,0 +1,20 @@
+package com.example.titto_backend.matchingBoard.controller;
+
+import com.example.titto_backend.matchingBoard.dto.request.MatchingPostRequest.MatchingPostPagingRequestDto;
+import com.example.titto_backend.matchingBoard.dto.response.matchingPostResponse.MatchingPostPagingResponseDto;
+import com.example.titto_backend.matchingBoard.service.matchingBoard.MatchingBoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/matching-board")
+@RequiredArgsConstructor
+public class MatchingBoardController {
+    private final MatchingBoardService matchingBoardService;
+
+    @GetMapping("/posts")
+    public MatchingPostPagingResponseDto getAllMatchingPosts(MatchingPostPagingRequestDto matchingPostPagingRequestDto) {
+        return matchingBoardService.findAllPosts(matchingPostPagingRequestDto);
+    }
