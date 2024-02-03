aebbe460dfecaaeb31c75f677d0874317e7ec47c
dongkyunKim <kyun010713@gmail.com>
2024-02-04 00:39:08 +0900

    feat(comtroller) : MatchingBoardController 추가

--- Titto_Backend/src/main/java/com/example/titto_backend/matchingBoard/controller/MatchingBoardController.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/matchingBoard/controller/MatchingBoardController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/matching-board")
 @RequiredArgsConstructor
+
 public class MatchingBoardController {
     private final MatchingBoardService matchingBoardService;
 
