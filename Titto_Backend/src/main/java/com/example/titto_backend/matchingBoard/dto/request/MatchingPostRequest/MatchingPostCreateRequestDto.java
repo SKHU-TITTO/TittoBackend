68b4a03731bc1b8d450b03a12578d53dcd741cf0
dongkyunKim <kyun010713@gmail.com>
2024-02-04 00:37:22 +0900

    fix(dto) : status 추가

--- Titto_Backend/src/main/java/com/example/titto_backend/matchingBoard/dto/request/MatchingPostRequest/MatchingPostCreateRequestDto.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/matchingBoard/dto/request/MatchingPostRequest/MatchingPostCreateRequestDto.java
@@ -2,6 +2,7 @@ package com.example.titto_backend.matchingBoard.dto.request.MatchingPostRequest;
 
 import com.example.titto_backend.matchingBoard.domain.matchingBoard.Category;
 import com.example.titto_backend.matchingBoard.domain.matchingBoard.MatchingPost;
+import com.example.titto_backend.matchingBoard.domain.matchingBoard.Status;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 
@@ -9,7 +10,7 @@ import lombok.AllArgsConstructor;
 public class MatchingPostCreateRequestDto {
 
     @NotNull
-    private Category category;
+    private String category;
 
     @NotNull
     private String title;
@@ -17,9 +18,13 @@ public class MatchingPostCreateRequestDto {
     @NotNull
     private String content;
 
+    @NotNull
+    private String status;
+
     public MatchingPost toEntity() {
         return MatchingPost.builder()
-                .category(category)
+                .category(Category.valueOf(category))
+                .status(Status.valueOf(status))
                 .title(title)
                 .content(content)
                 .build();
--- Titto_Backend/src/main/java/com/example/titto_backend/matchingBoard/dto/request/MatchingPostRequest/MatchingPostUpdateRequestDto.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/matchingBoard/dto/request/MatchingPostRequest/MatchingPostUpdateRequestDto.java
@@ -2,11 +2,12 @@ package com.example.titto_backend.matchingBoard.dto.request.MatchingPostRequest;
 
 import com.example.titto_backend.matchingBoard.domain.matchingBoard.Category;
 import com.example.titto_backend.matchingBoard.domain.matchingBoard.MatchingPost;
+import com.example.titto_backend.matchingBoard.domain.matchingBoard.Status;
 import jakarta.validation.constraints.NotNull;
 
 public class MatchingPostUpdateRequestDto {
     @NotNull
-    private Category category;
+    private String category;
 
     @NotNull
     private String title;
@@ -14,9 +15,13 @@ public class MatchingPostUpdateRequestDto {
     @NotNull
     private String content;
 
+    @NotNull
+    private String status;
+
     public MatchingPost toEntity() {
         return MatchingPost.builder()
-                .category(category)
+                .category(Category.valueOf(category))
+                .status(Status.valueOf(status))
                 .title(title)
                 .content(content)
                 .build();
