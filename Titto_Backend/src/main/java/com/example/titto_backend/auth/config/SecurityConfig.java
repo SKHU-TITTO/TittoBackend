2e0ebc383851b41494c065b3c9a27567a5e266af
dd-jiyun <dlwldbs8015@gmail.com>
2024-02-11 22:24:34 +0900

    fix: config

--- Titto_Backend/src/main/java/com/example/titto_backend/auth/config/SecurityConfig.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/auth/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
   public CorsConfigurationSource configurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
 
-    configuration.setAllowedOriginPatterns(List.of("http://localhost:8080","http://localhost:3000/", "https://titto.world/"));
+    configuration.setAllowedOriginPatterns(List.of("http://localhost:8080","http://localhost:3000", "https://titto.world"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setExposedHeaders(List.of("Access-Control-Allow-Credentials", "Authorization", "Set-Cookie"));
