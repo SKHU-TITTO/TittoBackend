fe8f1cec2f381a2d0194ef52d3aac8e793526aaf
dongkyunKim <kyun010713@gmail.com>
2024-02-18 21:28:45 +0900

    fix() : scripts 재수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="/home/ubuntu/Titto_Backend-0.0.1-SNAPSHOT.jar"
+JAR_FILE="/home/ubuntu/app/Titto_Backend/build/libs/Titto_Backend-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-# build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/Titto_Backend-0.0.1-SNAPSHOT.jar /home/ubuntu/
+TIME_NOW=$(date +%c)
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> "$DEPLOY_LOG"
 cd /home/ubuntu/app/Titto_Backend/build/libs
 nohup java -jar "$JAR_FILE" >> /dev/null 2>&1 &
-
 
 sleep 5  # 프로세스가 실행되기를 기다립니다. 필요한 경우 조정하세요.
 
