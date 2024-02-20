110dae8d665f7aa7e7370ce8a803462761ed1fe7
dongkyunKim <kyun010713@gmail.com>
2024-02-21 00:58:26 +0900

    fix() : 프로필 이미지 수정

--- Titto_Backend/src/main/java/com/example/titto_backend/matchingBoard/dto/response/matchingPostResponse/MatchingPostResponseDto.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/matchingBoard/dto/response/matchingPostResponse/MatchingPostResponseDto.java
@@ -1,12 +1,11 @@
 package com.example.titto_backend.matchingBoard.dto.response.matchingPostResponse;
 
 import com.example.titto_backend.matchingBoard.domain.matchingBoard.MatchingPost;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
@@ -16,6 +15,7 @@ public class MatchingPostResponseDto {
 
     private Long matchingPostId;
     private String authorNickName;
+    private String profile;
     private String category;
     private String status;
     private String title;
@@ -24,12 +24,13 @@ public class MatchingPostResponseDto {
     private Integer reviewCount;
     private LocalDateTime updateDate;
 
-    public static  MatchingPostResponseDto of(
+    public static MatchingPostResponseDto of(
             MatchingPost matchingPost,
             Integer reviewCount) {
         return new MatchingPostResponseDto(
                 matchingPost.getMatchingPostId(),
                 matchingPost.getUser().getNickname(),
+                matchingPost.getUser().getProfile(),
                 String.valueOf(matchingPost.getCategory()),
                 String.valueOf(matchingPost.getStatus()),
                 matchingPost.getTitle(),
