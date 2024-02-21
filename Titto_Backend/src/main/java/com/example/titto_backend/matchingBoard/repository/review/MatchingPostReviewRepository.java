8cae079752a11d78af393954300c87e67a847806
dongkyunKim <kyun010713@gmail.com>
2024-02-22 03:22:10 +0900

    fix() : MatchingPostReviewRepository 수정

--- Titto_Backend/src/main/java/com/example/titto_backend/matchingBoard/repository/review/MatchingPostReviewRepository.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/matchingBoard/repository/review/MatchingPostReviewRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 @Repository
 public interface MatchingPostReviewRepository extends JpaRepository<MatchingPostReview, Long> {
     List<MatchingPostReview> findAllByMatchingPost(MatchingPost matchingPost);
+
+    void deleteAllByMatchingPost(MatchingPost matchingPost);
 }
