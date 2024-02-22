3254fbe2651779401cb8d6f2be3762d4be94d7c4
dongkyunKim <kyun010713@gmail.com>
2024-02-23 01:36:23 +0900

    fix(User) : 누적 경험치, 현재 경험치 추가

--- Titto_Backend/src/main/java/com/example/titto_backend/auth/domain/User.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/auth/domain/User.java
@@ -6,24 +6,12 @@ import com.example.titto_backend.matchingBoard.domain.matchingBoard.MatchingPost
 import com.example.titto_backend.matchingBoard.domain.review.MatchingPostReview;
 import com.example.titto_backend.message.domain.Message;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
 import java.util.List;
 import java.util.Set;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Getter
@@ -31,79 +19,89 @@ import lombok.Setter;
 @NoArgsConstructor
 public class User extends BaseEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "user_id")
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
 
-  @Column(name = "email")
-  @NotBlank(message = "이메일은 필수!")
-  private String email;
+    @Column(name = "email")
+    @NotBlank(message = "이메일은 필수!")
+    private String email;
 
-  @Column(name = "profile", nullable = true)
-  private String profile;
+    @Column(name = "profile", nullable = true)
+    private String profile;
 
-  @Setter
-  @Column(name = "name")
-  private String name;
+    @Setter
+    @Column(name = "name")
+    private String name;
 
-  @Setter
-  @Column(name = "nickname")
-  private String nickname;
+    @Setter
+    @Column(name = "nickname")
+    private String nickname;
 
-  @Setter
-  @Column(name = "student_no")
-  private String studentNo;
+    @Setter
+    @Column(name = "student_no")
+    private String studentNo;
 
-  @Setter
-  @Column(name = "department")
-  private String department;
+    @Setter
+    @Column(name = "department")
+    private String department;
 
-  @Column(name = "social_id")
-  private String socialId;
+    @Column(name = "social_id")
+    private String socialId;
 
-  @Enumerated(EnumType.STRING)
-  private SocialType socialType;
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
 
-  @Setter
-  @Column(name = "one_line_intro")
-  private String oneLineIntro;
+    @Setter
+    @Column(name = "one_line_intro")
+    private String oneLineIntro;
 
-  @Setter
-  @Column(name = "self_intro", columnDefinition = "TEXT")
-  private String selfIntro;
+    @Setter
+    @Column(name = "self_intro", columnDefinition = "TEXT")
+    private String selfIntro;
 
-  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-  private Set<Badge> badges;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Badge> badges;
 
-  @JsonIgnore
-  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-  private List<MatchingPost> matchingPosts;
+    @JsonIgnore
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<MatchingPost> matchingPosts;
 
-  @JsonIgnore
-  @OneToMany(mappedBy = "reviewAuthor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-  private List<MatchingPostReview> matchingPostReviews;
+    @JsonIgnore
+    @OneToMany(mappedBy = "reviewAuthor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<MatchingPostReview> matchingPostReviews;
 
-  @JsonIgnore
-  @OneToMany(mappedBy = "sender", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-  private List<Message> sentMessages;
+    @JsonIgnore
+    @OneToMany(mappedBy = "sender", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Message> sentMessages;
 
-  @JsonIgnore
-  @OneToMany(mappedBy = "receiver", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-  private List<Message> receivedMessages;
+    @JsonIgnore
+    @OneToMany(mappedBy = "receiver", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Message> receivedMessages;
 
-  @Builder
-  public User(String email, String profile, String socialId, SocialType socialType) {
-    this.email = email;
-    this.profile = profile;
-    this.socialId = socialId;
-    this.socialType = socialType;
-  }
+    @Setter
+    @Column(name = "total_experience")
+    private Integer totalExperience; // 사용자의 누적 경험치
 
-  public void signupUser(SignUpDTO signUpDTO) {
-    this.setName(signUpDTO.getName());
-    this.setNickname(signUpDTO.getNickname());
-    this.setStudentNo(signUpDTO.getStudentNo());
-    this.setDepartment(signUpDTO.getDepartment());
-  }
+    @Setter
+    @Column(name = "current_experience")
+    private Integer currentExperience; // 사용자의 현재 경험치
+
+    @Builder
+    public User(String email, String profile, String socialId, SocialType socialType) {
+        this.email = email;
+        this.profile = profile;
+        this.socialId = socialId;
+        this.socialType = socialType;
+    }
+
+    public void signupUser(SignUpDTO signUpDTO) {
+        this.setName(signUpDTO.getName());
+        this.setNickname(signUpDTO.getNickname());
+        this.setStudentNo(signUpDTO.getStudentNo());
+        this.setDepartment(signUpDTO.getDepartment());
+        this.setTotalExperience(0);
+        this.setCurrentExperience(0);
+    }
 }
