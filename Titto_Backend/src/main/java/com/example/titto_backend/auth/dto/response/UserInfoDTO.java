ef920b6b62d081b6ab4ab0d7424e4d04281ee0c5
dongkyunKim <kyun010713@gmail.com>
2024-02-23 01:36:59 +0900

    fix(UserInfoDTo) : 누적 경험치, 현재 경험치 추가

--- Titto_Backend/src/main/java/com/example/titto_backend/auth/dto/response/UserInfoDTO.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/auth/dto/response/UserInfoDTO.java
@@ -11,11 +11,15 @@ public class UserInfoDTO {
   private String nickname;
   private String email;
   private String socialType;
+  private Integer totalExperience;
+  private Integer currentExperience;
 
   public UserInfoDTO(User user) {
     this.ProfileImg = user.getProfile();
     this.nickname = user.getNickname();
     this.email = user.getEmail();
     this.socialType = user.getSocialType().toString();
+    this.totalExperience = user.getTotalExperience();
+    this.currentExperience = user.getCurrentExperience();
   }
 }
