3ad71e0294ec277f9bd99d129b291268eb45497a
dongkyunKim <kyun010713@gmail.com>
2024-02-24 16:28:07 +0900

    feat() : 검색 기능 추가

--- Titto_Backend/src/main/java/com/example/titto_backend/matchingBoard/repository/matchingBoard/MatchingPostRepository.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/matchingBoard/repository/matchingBoard/MatchingPostRepository.java
@@ -1,9 +1,12 @@
 package com.example.titto_backend.matchingBoard.repository.matchingBoard;
 
 import com.example.titto_backend.matchingBoard.domain.matchingBoard.MatchingPost;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MatchingPostRepository extends JpaRepository<MatchingPost, Long> {
+    Page<MatchingPost> findByTitleContaining(String keyword, Pageable pageable);
 }
