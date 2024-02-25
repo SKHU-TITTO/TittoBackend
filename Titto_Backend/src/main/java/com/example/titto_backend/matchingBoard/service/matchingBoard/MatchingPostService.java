531ba89297f1669c1b3d8cb5d098f4509a6999b4
dongkyunKim <kyun010713@gmail.com>
2024-02-26 02:52:43 +0900

    refactor() : 매칭게시글 서비스 리팩토링

--- Titto_Backend/src/main/java/com/example/titto_backend/matchingBoard/service/matchingBoard/MatchingPostService.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/matchingBoard/service/matchingBoard/MatchingPostService.java
@@ -2,6 +2,8 @@ package com.example.titto_backend.matchingBoard.service.matchingBoard;
 
 import com.example.titto_backend.auth.domain.User;
 import com.example.titto_backend.auth.repository.UserRepository;
+import com.example.titto_backend.common.exception.CustomException;
+import com.example.titto_backend.common.exception.ErrorCode;
 import com.example.titto_backend.matchingBoard.domain.matchingBoard.Category;
 import com.example.titto_backend.matchingBoard.domain.matchingBoard.MatchingPost;
 import com.example.titto_backend.matchingBoard.domain.matchingBoard.Status;
@@ -18,7 +20,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.AuthorizationServiceException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,10 +34,7 @@ public class MatchingPostService {
     @Transactional
     public MatchingPostCreateResponseDto createMatchingPost(Principal principal,
                                                             MatchingPostCreateRequestDto matchingPostCreateRequestDto) {
-        String userEmail = principal.getName();
-        User user = userRepository.findByEmail(userEmail).orElseThrow(
-                () -> new IllegalArgumentException("존재하지 않는 사용자입니다"));
-
+        User user = getCurrentUser(principal);
         MatchingPost matchingPost = matchingPostCreateRequestDto.toEntity(user);
         matchingPostRepository.save(matchingPost);
 
@@ -44,23 +42,20 @@ public class MatchingPostService {
     }
 
     // 게시물 조회
+    @Transactional(readOnly = true)
     public MatchingPostResponseDto getMatchingPostByMatchingPostId(Long matchingPostId, HttpServletRequest request,
                                                                    HttpServletResponse response) {
-        MatchingPost matchingPost = matchingPostRepository.findById(matchingPostId).orElseThrow(
-                () -> new IllegalArgumentException("존재하지 않는 게시물입니다"));
-
-        Integer reviewCount = matchingPostReviewRepository.findAllByMatchingPost(matchingPost).size();  // 댓글 수
-        countViews(matchingPost, request, response); // 조회수 업데이트
-        matchingPostRepository.save(matchingPost);
-
+        MatchingPost matchingPost = findMatchingPostById(matchingPostId);
+        Integer reviewCount = matchingPostReviewRepository.countByMatchingPost(matchingPost);
+        updatePostViews(request, response, matchingPostId);
         return MatchingPostResponseDto.of(matchingPost, reviewCount);
     }
 
     // 게시물 삭제
     @Transactional
     public MatchingPostDeleteResponseDto deleteMatchingPostByMatchingPostId(Long matchingPostId) {
-        MatchingPost matchingPost = matchingPostRepository.findById(matchingPostId).orElseThrow(
-                () -> new IllegalArgumentException("존재하지 않는 게시물입니다"));
+        MatchingPost matchingPost = findMatchingPostById(matchingPostId);
+
         matchingPostReviewRepository.deleteAllByMatchingPost(matchingPost);
         matchingPostRepository.delete(matchingPost);
         return MatchingPostDeleteResponseDto.of(matchingPostId);
@@ -70,14 +65,10 @@ public class MatchingPostService {
     @Transactional
     public MatchingPostUpdateResponseDto updateMatchingPost(Long matchingPostId, Principal principal,
                                                             MatchingPostUpdateRequestDto matchingPostUpdateRequestDto) {
-        MatchingPost matchingPost = matchingPostRepository.findById(matchingPostId).orElseThrow(
-                () -> new IllegalArgumentException("존재하지 않는 게시물입니다"));
+        MatchingPost matchingPost = findMatchingPostById(matchingPostId);
 
-        String userEmail = principal.getName();
-        User user = userRepository.findByEmail(userEmail).orElseThrow(
-                () -> new IllegalArgumentException("존재하지 않는 사용자입니다"));
-
-        Integer reviewCount = matchingPostReviewRepository.findAllByMatchingPost(matchingPost).size();  // 댓글 수
+        User user = getCurrentUser(principal);
+        Integer reviewCount = matchingPostReviewRepository.countByMatchingPost(matchingPost);
 
         if (user.getNickname().equals(matchingPost.getUser().getNickname())) {
             // 게시물 내용 수정
@@ -92,47 +83,56 @@ public class MatchingPostService {
                     matchingPost,
                     reviewCount);
         } else {
-            throw new AuthorizationServiceException("잘못된 접근입니다");
+            throw new CustomException(ErrorCode.INVALID_ACCESS);
         }
     }
 
     // 게시글 조회수 연산
-    private void countViews(MatchingPost matchingPost, HttpServletRequest request, HttpServletResponse response) {
-        Cookie cookie = null;
-        Cookie[] cookies = request.getCookies();
-        if (matchingPost != null) {
-            String postId = "POST[" + matchingPost.getMatchingPostId() + "]";
-            if (cookies != null) {
-                // 쿠키 중에서 "postViews" 쿠키를 찾습니다.
-                for (Cookie oldCookie : cookies) {
-                    if (oldCookie.getName().equals("postViews")) {
-                        cookie = oldCookie;
-                        break;
-                    }
-                }
-            }
-            if (cookie != null) {
-                // "postViews" 쿠키가 있는 경우
-                if (!cookie.getValue().contains(postId)) {
-                    // 쿠키의 값에 현재 게시물의 ID가 없다면 조회수를 증가시키고 쿠키를 업데이트합니다.
-                    matchingPost.updateViewCount();
-                    cookie.setValue(cookie.getValue() + postId);
-                }
-            } else {
-                // "postViews" 쿠키가 없는 경우 쿠키를 생성하고 조회수를 증가시킵니다.
-                matchingPost.updateViewCount();
-                cookie = new Cookie("postViews", postId);
-            }
-            response.addCookie(setCookieValue(cookie));  // 쿠키를 응답에 추가합니다.
-            matchingPostRepository.save(matchingPost);  // 게시물을 저장하여 조회수를 업데이트합니다.
-        }
+    private void increaseViewCount(MatchingPost matchingPost) {
+        matchingPost.updateViewCount();
+        matchingPostRepository.save(matchingPost);
     }
 
-    // 쿠키 설정
-    private Cookie setCookieValue(Cookie cookie) {
-        cookie.setPath("/");
-        cookie.setMaxAge(60 * 60 * 24);
-        return cookie;
+    private void createOrUpdatePostViewsCookie(HttpServletRequest request, HttpServletResponse response,
+                                               MatchingPost matchingPost) {
+        Cookie[] cookies = request.getCookies();
+        String postId = "POST[" + matchingPost.getMatchingPostId() + "]";
+        Cookie postViewsCookie = null;
+        if (cookies != null) {
+            for (Cookie oldCookie : cookies) {
+                if (oldCookie.getName().equals("postViews")) {
+                    postViewsCookie = oldCookie;
+                    break;
+                }
+            }
+        }
+        if (postViewsCookie == null) {
+            postViewsCookie = new Cookie("postViews", postId);
+        } else {
+            if (!postViewsCookie.getValue().contains(postId)) {
+                postViewsCookie.setValue(postViewsCookie.getValue() + postId);
+            }
+        }
+        postViewsCookie.setPath("/");
+        postViewsCookie.setMaxAge(60 * 60 * 24);
+        response.addCookie(postViewsCookie);
+    }
+
+    public void updatePostViews(HttpServletRequest request, HttpServletResponse response, Long matchingPostId) {
+        MatchingPost matchingPost = findMatchingPostById(matchingPostId);
+        increaseViewCount(matchingPost);
+        createOrUpdatePostViewsCookie(request, response, matchingPost);
+    }
+
+    private User getCurrentUser(Principal principal) {
+        String userEmail = principal.getName();
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    private MatchingPost findMatchingPostById(Long matchingPostId) {
+        return matchingPostRepository.findById(matchingPostId)
+                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
     }
 }
 
