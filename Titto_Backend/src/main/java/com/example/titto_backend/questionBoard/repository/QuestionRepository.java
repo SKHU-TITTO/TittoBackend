a7a6d45e070bfbb028bea9fabc2a7583221bb169
dongkyunKim <kyun010713@gmail.com>
2024-02-27 15:29:06 +0900

    fix() : 프로필 조회 수정

--- Titto_Backend/src/main/java/com/example/titto_backend/questionBoard/repository/QuestionRepository.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/questionBoard/repository/QuestionRepository.java
@@ -1,7 +1,9 @@
 package com.example.titto_backend.questionBoard.repository;
 
+import com.example.titto_backend.auth.domain.User;
 import com.example.titto_backend.questionBoard.domain.Department;
 import com.example.titto_backend.questionBoard.domain.Question;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,5 +15,5 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     boolean existsByIdAndAcceptedAnswerIsNotNull(Long id); // 채택된 답변이 있는지 확인
 
-    
+    List<Question> findQuestionByAuthor(User user);
 }
