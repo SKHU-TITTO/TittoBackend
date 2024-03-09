e4f397ff5b1cb2790883be302b0fe2c2da7dbbad
dongkyunKim <kyun010713@gmail.com>
2024-03-09 19:16:30 +0900

    fix() : 사용자 작성 댓글 조회 수정

--- Titto_Backend/src/main/java/com/example/titto_backend/questionBoard/repository/AnswerRepository.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/questionBoard/repository/AnswerRepository.java
@@ -4,14 +4,15 @@ import com.example.titto_backend.auth.domain.User;
 import com.example.titto_backend.questionBoard.domain.Answer;
 import com.example.titto_backend.questionBoard.dto.AnswerInfoDto;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface AnswerRepository extends CrudRepository<Answer, Long> {
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
-    @Query("SELECT new com.example.titto_backend.questionBoard.dto.AnswerInfoDto(a.id, a.content) FROM Answer a WHERE a.author = :user")
-    List<AnswerInfoDto> findAnswerInfoByAuthor(@Param("user") User user);
+    @Query("SELECT new com.example.titto_backend.questionBoard.dto.AnswerInfoDto(a.id, a.content, a.question.id, a.question.title) FROM Answer a WHERE a.author = :user")
+    List<AnswerInfoDto> findAnswersInfoByAuthor(@Param("user") User user);
+
 
     List<Answer> findByQuestionId(Long questionId);
 }
