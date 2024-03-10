7b1c29bd3c285b36913913f7711cce26f9ec6249
dongkyunKim <kyun010713@gmail.com>
2024-03-10 17:42:13 +0900

    사용자 작성 게시글 조회 수정

--- Titto_Backend/src/main/java/com/example/titto_backend/matchingBoard/repository/matchingBoard/MatchingPostRepository.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/matchingBoard/repository/matchingBoard/MatchingPostRepository.java
@@ -3,10 +3,13 @@ package com.example.titto_backend.matchingBoard.repository.matchingBoard;
 import com.example.titto_backend.auth.domain.User;
 import com.example.titto_backend.matchingBoard.domain.matchingBoard.Category;
 import com.example.titto_backend.matchingBoard.domain.matchingBoard.MatchingPost;
+import com.example.titto_backend.matchingBoard.dto.response.matchingPostResponse.MatchingPostInfoDto;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,6 +18,9 @@ public interface MatchingPostRepository extends JpaRepository<MatchingPost, Long
 
     Page<MatchingPost> findByCategory(Category category, Pageable pageable);
 
-    List<MatchingPost> findMatchingPostByUser(User user);
+    @Query("SELECT new com.example.titto_backend.matchingBoard.dto.response.matchingPostResponse.MatchingPostInfoDto(a.matchingPostId, a.title, a.content, a.createDate, a.viewCount, a.reviewCount) "
+            + "FROM MatchingPost a"
+            + " WHERE a.user = :user")
+    List<MatchingPostInfoDto> findMatchingPostsInfoByAuthor(@Param("user") User user);
 
 }
--- Titto_Backend/src/main/java/com/example/titto_backend/questionBoard/repository/QuestionRepository.java
+++ Titto_Backend/src/main/java/com/example/titto_backend/questionBoard/repository/QuestionRepository.java
@@ -4,10 +4,13 @@ import com.example.titto_backend.auth.domain.User;
 import com.example.titto_backend.questionBoard.domain.Department;
 import com.example.titto_backend.questionBoard.domain.Question;
 import com.example.titto_backend.questionBoard.domain.Status;
+import com.example.titto_backend.questionBoard.dto.QuestionInfoDto;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
     Page<Question> findByDepartmentOrderByCreateDateDesc(Pageable pageable, Department category);
@@ -21,6 +24,11 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
     Page<Question> findQuestionByStatus(Status status, Pageable pageable);
 
     Page<Question> findByTitleContaining(String keyWord, Pageable pageable);
+
+    @Query("SELECT new com.example.titto_backend.questionBoard.dto.QuestionInfoDto(a.id, a.title, a.content, a.createDate, a.viewCount, a.answerCount) "
+            + "FROM Question a"
+            + " WHERE a.author = :user")
+    List<QuestionInfoDto> findQuestionsInfoByAuthor(@Param("user") User user);
 
 
 }
