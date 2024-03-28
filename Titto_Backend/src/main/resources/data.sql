d4d280980a338e43b62dfa3f2c0b492f282b1f3f
dongkyunKim <kyun010713@gmail.com>
2024-03-28 16:08:17 +0900

    feat() : 더미데이터 추가

---
+++ Titto_Backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (user_id, name, profile, create_date, update_date, level)
+values (1, '알 수 없음',
+        'https://mblogthumb-phinf.pstatic.net/MjAyMDExMDFfMTgy/MDAxNjA0MjI4ODc1NDMw.Ex906Mv9nnPEZGCh4SREknadZvzMO8LyDzGOHMKPdwAg.ZAmE6pU5lhEdeOUsPdxg8-gOuZrq_ipJ5VhqaViubI4g.JPEG.gambasg/%EC%9C%A0%ED%8A%9C%EB%B8%8C_%EA%B8%B0%EB%B3%B8%ED%94%84%EB%A1%9C%ED%95%84_%ED%95%98%EB%8A%98%EC%83%89.jpg?type=w800'
+           , '2024-03-15 17:15:21.711342', '2024-03-15 17:15:21.711342', 0);
